5f20def99305878a12cfda40ccadc437de22419b
崔 明辉 <cuis@vip.qq.com>
2021-01-21 15:24:36 +0800

    fix: Add html for backup deviceinfo.

--- lib/ui/src/mock_engine/device_info_js.dart
+++ lib/ui/src/mock_engine/device_info_js.dart
@@ -1,13 +1,44 @@
 import 'dart:js' as js;
+import 'dart:html' as html;
 
 class DeviceInfo {
-  static double physicalSizeWidth = js.context['document']['body']
-          ['clientWidth'] /
-      js.context['devicePixelRatio'];
-  static double physicalSizeHeight = js.context['document']['body']
-          ['clientHeight'] /
-      js.context['devicePixelRatio'];
-  static double devicePixelRatio = js.context['devicePixelRatio'];
+  static double physicalSizeWidth = (() {
+    try {
+      return js.context['document']['body']['clientWidth'] /
+          js.context['devicePixelRatio'];
+    } catch (e) {
+      try {
+        return (html.document.body?.clientWidth ?? 0.0) *
+            html.window.devicePixelRatio;
+      } catch (e) {
+        return 0.0;
+      }
+    }
+  })();
+  static double physicalSizeHeight = (() {
+    try {
+      return js.context['document']['body']['clientHeight'] /
+          js.context['devicePixelRatio'];
+    } catch (e) {
+      try {
+        return (html.document.body?.clientHeight ?? 0.0) *
+            html.window.devicePixelRatio;
+      } catch (e) {
+        return 0.0;
+      }
+    }
+  })();
+  static double devicePixelRatio = (() {
+    try {
+      return js.context['devicePixelRatio'];
+    } catch (e) {
+      try {
+        return html.window.devicePixelRatio;
+      } catch (e) {
+        return 1.0;
+      }
+    }
+  })();
 
   static void listenDeviceSizeChanged(Function callback) {
     js.context.callMethod('addEventListener', [
