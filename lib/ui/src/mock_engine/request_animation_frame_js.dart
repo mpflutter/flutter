576b4b0f9a407e08e435853e12a36d6ea5feec8e
崔 明辉 <cuis@vip.qq.com>
2021-02-09 18:46:44 +0800

    feat: no use html.window.requestAnimationFrame for taro.

--- lib/ui/src/mock_engine/request_animation_frame_js.dart
+++ lib/ui/src/mock_engine/request_animation_frame_js.dart
@@ -1,5 +1,14 @@
 import 'dart:html' as html;
 
+const bool isTaro =
+    bool.fromEnvironment('mpcore.env.taro', defaultValue: false);
+
 void requestAnimationFrame(Function(num) callback) {
+  if (isTaro) {
+    Future.delayed(Duration(milliseconds: 16)).then((value) {
+      callback(DateTime.now().millisecondsSinceEpoch);
+    });
+    return;
+  }
   html.window.requestAnimationFrame(callback);
 }
