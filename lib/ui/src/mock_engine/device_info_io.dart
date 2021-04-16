c212a800670e0f632ec10086b3add2f1882dc473
崔 明辉 <cuis@vip.qq.com>
2021-04-16 15:21:37 +0800

    feat: use 603 height for taro.

--- lib/ui/src/mock_engine/device_info_io.dart
+++ lib/ui/src/mock_engine/device_info_io.dart
@@ -1,6 +1,11 @@
+static const bool isTaro = bool.fromEnvironment(
+  'mpcore.env.taro',
+  defaultValue: false,
+);
+
 class DeviceInfo {
   static double physicalSizeWidth = 375.0 * 2.0;
-  static double physicalSizeHeight = 667.0 * 2.0;
+  static double physicalSizeHeight = (isTaro ? 603.0 : 667.0) * 2.0;
   static double devicePixelRatio = 2.0;
   static void listenDeviceSizeChanged(Function callback) {}
 }
